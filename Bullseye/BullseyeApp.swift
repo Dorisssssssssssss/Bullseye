058c1b88157987e8cb459e4eff3a2f51df4b0eba
Dorisssssssssssss <xtao65@gatech.edu>
2025-09-04 00:39:49 -0400

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by XIN on 9/4/25.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
