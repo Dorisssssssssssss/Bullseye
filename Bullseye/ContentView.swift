583905b3cc0b8cba004455569c0c1aadcddbe4a5
Dorisssssssssssss <xtao65@gatech.edu>
2025-09-04 03:06:15 -0400

    Slider

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,25 +9,66 @@ import SwiftUI
 
 struct ContentView: View {
     @State var alertIsVisible: Bool = false
+    @State var sliderValue: Double = 50.0
     //@State var whosThereVisible: Bool = false
 
     var body: some View {
         VStack {
-            Text("Welcome to my first app!")
-                .fontWeight(.semibold)
-                .foregroundColor(Color.green)
-            Button(action: {
-                print("Button pressed!")
-                self.alertIsVisible = true
-            }) {
-                Text("Hit Me!")
+            VStack {
+                Spacer()
+                //target row
+                HStack {
+                    Text("Put the bullseye as close as you can to:")
+                    Text("100")
+                        //.fontWeight(.semibold)
+                        //.foregroundColor(Color.green)
+                }
+                Spacer()
+                
+             //Slider row
+                HStack{
+                    Text("1")
+                    Slider(value:self.$sliderValue, in: 1...100)
+                    Text("100")
+                }
+                
+                //Button row
+                Button(action: {
+                    print("Button pressed!")
+                    self.alertIsVisible = true
+                }) {
+                    Text("Hit Me!")
+                }
+                Spacer()
             }
+            
+            .alert(isPresented: $alertIsVisible) { () -> Alert in
+                var roundedValue: Int = Int(self.sliderValue.rounded())
+                return Alert(title: Text("Hello there!"),
+                             message: Text("The slider's value is \(roundedValue)"),
+                             dismissButton: .default(Text("Awesome!")))
+            }
+            Spacer()
+            
+            //Score Row
+            HStack{
+                Button(action:{}) {
+                    Text("Start Over")
+                }
+                Spacer()
+                Text("Score")
+                Text("999999")
+                Spacer()
+                Text("Round")
+                Text("999")
+                Spacer()
+                Button(action:{}) {
+                    Text("Info")
+                }
+            }
+            .padding(.bottom,20)
         }
-        .alert(isPresented: $alertIsVisible) { () -> Alert in
-            return Alert(title: Text("Hello there!"),
-                         message: Text("This is my first pop-up."),
-                         dismissButton: .default(Text("Awesome!")))
-        }
+        
         /*Button(action:{
             self.whosThereVisible = true
         }) {
