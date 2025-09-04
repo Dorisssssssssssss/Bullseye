5c63a21f9d156d68ac2130e485ea2ac06869cb72
Dorisssssssssssss <xtao65@gatech.edu>
2025-09-04 01:16:45 -0400

    feat: message block

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var alertIsVisible: Bool = false
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Welcome to my first app!")
+                .fontWeight(.semibold)
+                .foregroundColor(Color.green)
+            Button(action: {
+                print("Button pressed!")
+                self.alertIsVisible = true
+            }) {
+                Text("Hit Me!")
+            }
         }
-        .padding()
+        .alert(isPresented: $alertIsVisible) { () -> Alert in
+            return Alert(title: Text("Hello there!"),
+                         message: Text("This is my first pop-up."),
+                         dismissButton: .default(Text("Awesome!")))
+        }
     }
 }
 
